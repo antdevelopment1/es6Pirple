3ab5326e4aa8a1678ef838005d877036e2427044
April Copes <antdevelopment1@gmail.com>
2019-01-22 22:09:15 -0500

    Simple log statement example

---
+++ script.js
@@ -0,0 +1,23 @@
+let count = 0;
+let color;
+
+while (count <= 100) {
+    console.log(count);
+    count++;
+}
+
+color = 'brown';
+
+if (color === 'green') {
+    console.log('Why hello there');
+} else {
+    console.log("Goodbye");
+}
+
+color = 'green';
+
+if (color === 'green') {
+    console.log('Why hello there');
+} else {
+    console.log("Goodbye");
+}
