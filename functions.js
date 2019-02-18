b45d8aac5477156b4e7796c141af750b8ff871e9
April Copes <antdevelopment1@gmail.com>
2019-02-18 07:50:42 -0500

    filter example

--- functions.js
+++ functions.js
@@ -41,3 +41,20 @@ const addStuff = (a, b) => a + b;
 console.log(addStuff(10,20));
 
 
+// Another option to do the same thing as map user name
+const anotherMapFunction = (arr) => arr.map(user => user.name)
+
+console.log(anotherMapFunction(users));
+
+
+// Filter function
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const getEvens = (arr) => {
+    const filteredNumbers = numbers.filter( (number) => {
+        return number % 2 === 0;
+    })
+
+    return filteredNumbers;
+}
+
+console.log(getEvens(numbers));
